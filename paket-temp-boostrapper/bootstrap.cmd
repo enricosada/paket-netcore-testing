fb2f3548c48a6e0a80e8d51a2d228ff73709968a
Enrico Sada <enrico@sada.io>
2018-01-10 17:20:21 +0100

    fix

--- paket-temp-boostrapper/bootstrap.cmd
+++ paket-temp-boostrapper/bootstrap.cmd
@@ -5,7 +5,7 @@ REM we want to use paket .net core
 set PAKET_REPOTOOL_PREFERRED_RUNTIME=netcoreapp
 
 REM install to download the paket as repo tool
-paket.exe install
+paket.exe install --verbose
 
 REM hack to override the paket path
 copy /Y Directory.Build.props.xml ..\Directory.Build.props
