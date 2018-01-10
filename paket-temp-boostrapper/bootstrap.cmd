7b1e7fc2aa11fc721e13260ccbfab78b41aae81f
Enrico Sada <enrico@sada.io>
2018-01-10 17:20:21 +0100

    doc

--- paket-temp-boostrapper/bootstrap.cmd
+++ paket-temp-boostrapper/bootstrap.cmd
@@ -1,13 +1,32 @@
+@ECHO OFF
 
 pushd %~dp0
+
+echo.
+echo we are going to boostrap paket as repo tool, using .net core framework
+echo so will download paket from Paket.nupkg
+echo using this temporary boostrapper
+echo.
 
 REM we want to use paket .net core
 set PAKET_REPOTOOL_PREFERRED_RUNTIME=netcoreapp
 
 REM install to download the paket as repo tool
+echo paket.exe install --verbose
 paket.exe install --verbose
 
 REM hack to override the paket path
+echo use this to override the default paket path
 copy /Y Directory.Build.props.xml ..\Directory.Build.props
+
+echo.
+echo DONE!
+echo.
+echo now you can use:
+echo.
+echo .paket-netcore\paket --help
+echo.
+echo NOTE this is just a symlink to paket-temp-boostrapper\paket-files\bin\paket
+echo.
 
 popd
